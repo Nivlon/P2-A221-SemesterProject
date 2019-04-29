b8cdf1d3d092049528fde4848bb5f48df69d5df7
Fabian Demeter <fabiandemeter22@gmail.com>
2019-04-29 15:05:52 +0200

    Tanja Learning Version
    Has errors

---
+++ app/src/main/java/com/example/p2semesterproject/FoodObject.java
@@ -0,0 +1,36 @@
+package com.example.p2semesterproject;
+
+
+
+public class FoodObject {
+
+    String name;
+    static String[] storageSpaces={"Fridge","Freezer","Cabinet"};
+    int[] storageTime=new int[storageSpaces.length];
+
+    FoodObject(String _name) {
+        name=_name;
+    }
+
+    FoodObject(String _name,int[] _storageTime) {
+        name=_name;
+        storageTime=_storageTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getStorageTime() {
+        return storageTime;
+    }
+
+    public void setStorageTime(int[] storageTime) {
+        this.storageTime = storageTime;
+    }
+
+}
--- app/src/main/java/com/example/p2semesterproject/MainActivity.java
+++ app/src/main/java/com/example/p2semesterproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.p2semesterproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// Capture our button from layout
+        Button fruitVegBut = findViewById(R.id.corky);
+        Button meatBut = findViewById(R.id.corky2);
+        Button DairyBut = findViewById(R.id.corky3);
+        // Register the onClick listener with the implementation above
+        fruitVegBut.setOnClickListener(mCorkyListener);
+        meatBut.setOnClickListener(mCorkyListener);
+        DairyBut.setOnClickListener(mCorkyListener);
+
     }
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener mCorkyListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            // Yes we will handle click here but which button clicked??? We don't know
+
+            // So we will make
+            switch (v.getId() /*to get clicked view id**/) {
+                case R.id.corky:
+
+                    // do something when the corky is clicked
+
+                    break;
+                case R.id.corky2:
+
+                    // do something when the corky2 is clicked
+
+                    break;
+                case R.id.corky3:
+
+                    // do something when the corky3 is clicked
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
 }
+
+
