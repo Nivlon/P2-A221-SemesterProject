332ea106b5268cbb25c088fba632e96b4ca597a2
MIkkel Sang Mee Baunsgaard <baunsgaard38@gmail.com>
2019-05-08 11:55:26 +0200

    New method for generating quiz in progress and made a onClick for the buttons

--- app/src/main/java/com/example/p2semesterproject/MainActivity.java
+++ app/src/main/java/com/example/p2semesterproject/MainActivity.java
@@ -7,13 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Quiz buttons
+        Button option_1 = findViewById(R.id.button);
+        Button option_2 = findViewById(R.id.button2);
+        Button option_3 = findViewById(R.id.button3);
+
+
+        // Quiz onClick listener
+        option_1.setOnClickListener(quizButtonListener);
+        option_2.setOnClickListener(quizButtonListener);
+        option_3.setOnClickListener(quizButtonListener);
+
 
         // Capture button from layout
         Button fruitBut = findViewById(R.id.fruit_category);
@@ -23,11 +39,50 @@ public class MainActivity extends AppCompatActivity {
         Button vegBut = findViewById(R.id.veg_category);
 
         // Register the onClick listener with the implementation above
-        fruitBut.setOnClickListener(categoryButtonListener);
+            fruitBut.setOnClickListener(categoryButtonListener);
         meatBut.setOnClickListener(categoryButtonListener);
         dairyBut.setOnClickListener(categoryButtonListener);
         bakedBut.setOnClickListener(categoryButtonListener);
         vegBut.setOnClickListener(categoryButtonListener);
+
+
+    }
+
+    private View.OnClickListener quizButtonListener = new View.OnClickListener(){
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        public void onClick(View v){
+
+            setContentView(R.layout.activity_main);
+            TextView newText;
+
+            switch(v.getId()){
+                case R.id.button:
+                    newText = (TextView)findViewById(R.id.textView2);
+                    newText.setText(Integer.toString(R.id.button));
+                    break;
+
+                case R.id.button2:
+                    newText = (TextView)findViewById(R.id.textView2);
+                    newText.setText(Integer.toString(R.id.button2));
+                    break;
+
+                case R.id.button3:
+                    newText = (TextView)findViewById(R.id.textView2);
+                    newText.setText(Integer.toString(R.id.button3));
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void quizGenerator(FoodObject food, TextView quiz, View see){
+        quiz.setText("Where do you store " + food.getName() + " ?");
+
+        switch(see.getId()) {
+
+
+        }
     }
 
     // Create an anonymous implementation of OnClickListener
